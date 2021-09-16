9c8437e79b89b05493492ff972f2fbe471269afe
Rishal7 <rishalk4@gmail.com>
2021-09-16 14:20:01 +0530

    moved bcrypt, jwt to user schema

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -9,7 +9,7 @@ import { UserModel } from "../../database/user";
 const Router = express.Router();
 
 /* 
-Route       /signup
+Route       /auth/signup
 desc        Register new user
 Params      none
 Access      Public
@@ -17,30 +17,13 @@ Method      POST
 */
 Router.post("/signup", async (req, res) => {
   try {
-    const { email, password, fullname, phonenumber } = req.body.credentials;
-    const checkUserByEmail = await UserModel.findOne({ email });
-    const checkUserByPhone = await UserModel.findOne({ phonenumber });
-
-    // Check whether email exists
-    if (checkUserByEmail || checkUserByPhone) {
-      return res.json({ email: "User already exists!" });
-    }
-
-    // Hash password
-    const bcryptSalt = await bcrypt.genSalt(8);
-    const hashedPassword = await bcrypt.hash(password, bcryptSalt);
-
-    // Save to DB
-    await UserModel.create({
-      ...req.body.credentials,
-      password: hashedPassword,
-    });
-
-    // Generate JWT auth token
-    const token = jwt.sign({ user: { fullname, email } }, "ZomatoApp");
-
+    await UserModel.findByEmailAndPhone(req.body.credentials);
+    const newUser = await UserModel.create(req.body.credentials);
+    const token = newUser.generateJwtToken();
     return res.status(200).json({ token, status: "success" });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 });
+
+export default Router;
